2fb077baa254e6215916f124f1e9fac411e3a47a
ernysans <ernysans@icloud.com>
2016-04-11 15:20:38 -0500

    Test

---
+++ index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// Export Module ======================================== //
+//module.exports = function () { 
+//	console.log('hi');
+//	console.log('new');
+//};
+
+//
+module.exports = {
+  sayHelloInEnglish: function() {
+    return "HELLO";
+  },
+       
+  sayHelloInSpanish: function() {
+    return "Hola";
+  }
+};
---
+++ test/index.js
@@ -0,0 +1,14 @@
+//var module = function(path) {
+//  return exports;
+//};
+//var require = function(path) {
+//  return module;
+//};
+
+
+var bring = require('../index');
+
+//bring();
+
+console.log(bring.sayHelloInEnglish());
+console.log(bring.sayHelloInSpanish());
